a7612c1c1f437fd583defd49d057bbadac7cfb19
Sergio <sergio.costa@outlook.com>
2020-09-12 13:32:11 -0300

    added imageconfig model and service

---
+++ src/services/imageConfig.service.ts
@@ -0,0 +1,14 @@
+import { ApiResponse } from '../interfaces/ApiResponseModel'
+import ApiService from './api.service'
+
+class ImageConfigService {
+
+  private baseUrl: string = 'configuration'
+
+  public getImageConfig(): Promise<ApiResponse> {
+    return ApiService.list(this.baseUrl)
+  }
+
+}
+
+export default new ImageConfigService()
