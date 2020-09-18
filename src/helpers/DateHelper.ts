c68827fdda221638a6b0a9d33dc3314c4f0302ec
Sergio <sergio.costa@outlook.com>
2020-09-17 21:23:53 -0300

    remove momentjs due to lack of importance

--- src/helpers/DateHelper.ts
+++ src/helpers/DateHelper.ts
@@ -1,9 +1,27 @@
-import moment from 'moment'
-
+const months = [
+  'Jan',
+  'Feb',
+  'Mar',
+  'Apr',
+  'May',
+  'Jun',
+  'Jul',
+  'Aug',
+  'Sep',
+  'Oct',
+  'Nov',
+  'Dec'
+]
 class DateHelper {
 
   public apiDateToHumanReadableFormat = (date: string): string => {
-    return moment(date, "YYYY-MM-DD").format("MMM D, YYYY")
+    const dateInstance = new Date(date+'T00:00')
+    const year = dateInstance.getFullYear()
+    const monthIndex = dateInstance.getMonth()
+    const month = months[monthIndex]
+    const day = dateInstance.getDate()
+
+    return `${month} ${day}, ${year}`
   }
 
 }
